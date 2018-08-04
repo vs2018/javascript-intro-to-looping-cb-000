f684a74f3fe9e5fe3af3b4e12024164e9feba47f
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 06:54:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,10 +21,12 @@ function whileLoop (number) {
   }
 }
 
-function doWhileLoop(array) {
-  function maybeTrue() {
-  return Math.random() >= 0.5
+function maybeTrue() {
+return Math.random() >= 0.5
 }
+
+function doWhileLoop(array) {
+  
 
 do {
   array.pop()
