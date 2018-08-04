a12ba5dc50f41d2e95f1f2875d1ef42133281d26
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 06:26:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@ function forLoop (arr){
   var newArr = []
   for (let i = 0; i < 25; i++) {
     if (i = 1) {
-      "I am 1 strange loop."
+      newArr.push("I am 1 strange loop.")
       else {
-        `I am ${i} strange loops.`
+        newArr.push(`I am ${i} strange loops.`)
       }
 
     }
