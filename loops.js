7ba0cb4b3ae12c6b35d1014fba6650e37beb11fc
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 06:25:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,3 +11,18 @@ function forLoop (arr){
   }
     return arr
 }
+
+describe('forLoop(array)', () => {
+  it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
+    const [array, t] = makeArray()
+    const strangeArray = forLoop(array)
+
+    expect(strangeArray.length).to.equal(t + 25)
+
+    const testArray = strangeArray.slice(array.length)
+
+    for (let i = 0, l = testArray.length; i < l; i++) {
+      let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+      expect(testArray[i]).to.equal(s)
+    }
+  })
